bf7c38f8480b56ec13ea244765943de1a3840c74
p.obischenko <pavel.obischenko@gmail.com>
2016-10-19 12:35:40 +0300

    - new activations added

---
+++ AstraNet/ActivationFunctions/ReLUActivationFunction.h
@@ -0,0 +1,23 @@
+//
+// Created by Pavel on 17/10/16.
+// Copyright (c) 2016 pavel. All rights reserved.
+//
+
+#ifndef ASTRA_NN_RELUACTIVATIONFUNCTION_H
+#define ASTRA_NN_RELUACTIVATIONFUNCTION_H
+
+#include "ActivationFunction.h"
+#include <algorithm>
+
+namespace astra {
+
+    class ReLUActivationFunction : public ActivationFunction {
+    public:
+        ReLUActivationFunction() {}
+        virtual double value(double arg) const { return std::max(arg, 0); }
+    };
+
+}
+
+
+#endif //ASTRA_NN_RELUACTIVATIONFUNCTION_H
---
+++ AstraNet/ActivationFunctions/SoftplusActivationFunction.h
@@ -0,0 +1,27 @@
+//
+// Created by Pavel on 17/10/16.
+// Copyright (c) 2016 pavel. All rights reserved.
+//
+
+#ifndef ASTRA_NN_RELUACTIVATIONFUNCTION_H
+#define ASTRA_NN_RELUACTIVATIONFUNCTION_H
+
+#include "ActivationFunction.h"
+#include <cmath>
+
+namespace astra {
+
+    class SoftplusActivationFunction : public ActivationFunction {
+    public:
+        SoftplusActivationFunction() {}
+        virtual double value(double arg) const {
+            return  std::log(1 + std::exp(arg));
+        }
+
+        virtual double derivativeValue(double arg) const {
+            return 1 / (1 + std::exp(-arg));
+        }
+    };
+}
+
+#endif //ASTRA_NN_RELUACTIVATIONFUNCTION_H
